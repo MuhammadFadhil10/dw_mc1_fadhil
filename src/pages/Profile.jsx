44f39e64bf53f2a936a521182419698215a9f141
muhammadfadhil10 <muhammadfadhilakbar10@gmail.com>
2022-10-21 15:08:41 +0700

    update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,7 +8,7 @@ const Profile = () => {
 	const profile = userData.filter((p) => p.id == id.id);
 	const navigate = useNavigate();
 	return (
-		<Container className='d-flex flex-column '>
+		<Container className='d-flex flex-column align-items-center'>
 			<Image
 				src={profile[0].image}
 				width='200px'
@@ -16,11 +16,24 @@ const Profile = () => {
 				className='rounded-pill shadow border border-danger'
 			></Image>
 			<h3>{profile[0].username}</h3>
-			<div>
-				<p>{profile[0].follower}</p>
-				<p>{profile[0].following}</p>
+			<div className='d-flex justify-content-between w-100 align-items-start'>
+				<div>
+					<p>{profile[0].follower}</p>
+					<p>Followers</p>
+				</div>
+				<div>
+					<p>{profile[0].following}</p>
+
+					<p>Following</p>
+				</div>
 			</div>
-			<p onClick={() => navigate(-1)} style={{ cursor: 'pointer' }}>
+			{/* <div className='d-flex justify-content-between w-100 gap-2'> */}
+			{/* </div> */}
+			<p
+				onClick={() => navigate(-1)}
+				style={{ cursor: 'pointer' }}
+				className='text-primary'
+			>
 				Back
 			</p>
 		</Container>
